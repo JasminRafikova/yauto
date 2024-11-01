5d3a67b74fa3773954893523a451d1cec2584e17
JasminRafikova <jasminarafikova2020@yandex.ru>
2024-11-01 10:09:37 -0200

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,56 @@
+export const products = [
+    {
+        id: 1,
+        img: '/card1.jpg',
+        name: 'первое',
+        price: 462
+    },
+    {
+        id: 2,
+        img: '/card2.jpg',
+        name: 'втопое',
+        price: '355'
+    },
+    {
+        id: 3,
+        img: '/card3.jpg',
+        name: 'третье',
+        price: 736
+    },
+    {
+        id: 4,
+        img: '/card4.jpg',
+        name: 'четверг',
+        price: 321
+    },
+    {
+        id: 5,
+        img: '/card5.jpg',
+        name: 'пятое',
+        price: 345
+    },
+    {
+        id: 6,
+        img: '/card6.jpg',
+        name: 'шесоте',
+        price: 432
+    },
+    {
+        id: 7,
+        img: '/card7.jpg',
+        name: 'седбмле',
+        price: 1753
+    },
+    {
+        id: 8,
+        img: '/card8.jpg',
+        name: 'восмьмое я не знаю че писать',
+        price: 513
+    },
+    {
+        id: 9,
+        img: '/card9.jpg',
+        name: 'девчьое',
+        price: 565
+    }
+]
